3ef66259b4cf959b4ad96e4559e36816e17956d5
DavidAkerlind <david.akerlind@icloud.com>
2025-05-21 13:19:53 +0200

    feat: add homepage and deployment scripts to package.json; update Vite config for base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
+// https://davidakerlind.github.io/harpaviljongen-admin-service/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	base: '/harpaviljongen-admin-service',
+});
